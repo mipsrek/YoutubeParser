df6e23c39ddc3ae4587a7132d6abcc51a98d2861
MatildeJPereira <tide2563@gmail.com>
2025-06-07 10:39:18 +0200

    added views, fixed issues

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tokenize import String
 from urllib.parse import urlparse, parse_qs
 
 from selenium import webdriver
+from selenium.common import NoSuchElementException, TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
@@ -12,11 +13,24 @@ from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 
+# TODO there is a "get youtube premium tab" in front of useful things sometimes. make a function to get rid of that.
+
 INITIAL_URL = "https://www.youtube.com/watch?v=CpCKkWMbmXU"
-DEPTH = 3
-NUMBER_RECOMMENDATIONS = 3
-KEY_WORDS = ["israel", "deep state", "climate change"]
-NUMBER_SEARCH = 1
+DEPTH = 5
+NUMBER_RECOMMENDATIONS = 5
+KEY_WORDS = ["hamas", "gaza occupation", "israel war"]
+NUMBER_SEARCH = 2
+
+
+# start a new webdriver altogether, useful for when the session breaks
+def start_new_driver():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--window-size=1920,1080")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.set_page_load_timeout(30)
+    return driver
+
 
 # avoid repeating webdriver waits
 def wait_for(driver, condition, timeout=60):
@@ -28,28 +42,36 @@ def accept_cookies(driver):
     print("Accepted cookies")
 
 
+
 def get_video_title(driver):
     video_title = wait_for(driver, EC.presence_of_element_located((By.ID, "title")))
     return video_title.text
-
-    # try:
-    #     video_title = (driver
-    #                    .find_element(By.ID, "title")
-    #                    .find_element(By.TAG_NAME, "yt-formatted-string").text)
-    # except:
-    #     video_title = "**Empty**"
-    # return video_title
-
-# # TODO get comments under video
-# def get_comments(driver):
-#     driver.execute_script("window.scrollTo(0, 600);")
-#     time.sleep(3)
-#     comments = driver.find_element(By.ID, "comments").find_elements(By.ID, "contents")
 
 
 def get_channel(driver):
     channel = wait_for(driver, EC.presence_of_element_located((By.XPATH, "//*[@id=\"text\"]/a")))
     return channel.text
+
+def get_views(driver):
+    # if the description isn't open yet, though it should
+    if driver.find_element(By.ID, "expand").is_displayed():
+        driver.find_element(By.ID, "expand").click()
+
+    # if the view-count element has the properties of a normal video, get the views from info element
+    # else this is a stream, so get the views from the view-count directly
+    info_container = wait_for(driver, EC.presence_of_element_located((By.ID, "info-container")))
+    view_count = info_container.find_element(By.ID, "view-count")
+
+    element = wait_for(view_count, EC.presence_of_element_located((By.XPATH, "//*[@id=\"view-count\"]/yt-animated-rolling-number")))
+    if element.get_attribute("style") == "":
+        views = wait_for(driver, EC.presence_of_element_located((By.XPATH, "//*[@id=\"info\"]/span[1]")))
+        views_num, _ = views.text.split(" ")
+    else:
+        print("This is a stream.")
+        views = view_count.text
+        views_num, _, _ = views.split(" ")
+
+    return views_num
 
 
 # TODO change to the actual number?
@@ -63,8 +85,11 @@ def get_likes(driver):
 
 
 def get_description(driver):
-    if driver.find_elements(By.ID, "expand"):
-        driver.find_element(By.ID, "expand").click()
+    if driver.find_element(By.ID, "expand").is_displayed():
+        wait_for(driver, EC.invisibility_of_element((By.ID, "tooltip")))
+        wait_for(driver, EC.element_to_be_clickable((By.ID, "expand"))).click()
+
+        # driver.find_element(By.ID, "expand").click()
         if driver.find_elements(By.XPATH, "//*[@id=\"description-inline-expander\"]/yt-attributed-string/span"):
             description_span = driver.find_element(By.XPATH, "//*[@id=\"description-inline-expander\"]/yt-attributed-string/span")
             return description_span.text
@@ -80,19 +105,16 @@ def get_transcript(driver):
         driver.find_element(By.ID, "expand").click()
 
     # click on show transcript if exists
-    if driver.find_elements(By.XPATH, "//*[@id=\"primary-button\"]/ytd-button-renderer/yt-button-shape/button"):
-        transcript_button = driver.find_element(By.XPATH, "//*[@id=\"primary-button\"]/ytd-button-renderer/yt-button-shape/button")
-        transcript_button.click()
-
+    try:
+        wait_for(driver, EC.element_to_be_clickable((By.XPATH, "//*[@id=\"primary-button\"]/ytd-button-renderer/yt-button-shape/button")), 10).click()
         try:
             raw_transcript = wait_for(driver, EC.presence_of_element_located((By.ID, "segments-container")))
-            transcript_lines = raw_transcript.find_elements(By.TAG_NAME, "yt-formatted-string")
+            transcript_lines = wait_for(raw_transcript, EC.presence_of_all_elements_located((By.TAG_NAME, "yt-formatted-string")))
             transcript = " ".join([transcript_line.text for transcript_line in transcript_lines])
         except:
             print("Transcript took too long to load")
             transcript = "Empty"
-
-    else:
+    except:
         print("No transcript available")
         transcript = "Empty"
 
@@ -142,9 +164,13 @@ def get_video_id(url):
 def get_recommended(driver):
     recs_links = []
     try:
-        recs = (driver.find_element(By.ID, 'related')
-                .find_element(By.ID, 'items')
-                .find_elements(By.TAG_NAME, "ytd-compact-video-renderer"))
+        related = wait_for(driver, EC.presence_of_element_located((By.ID, 'related')))
+        items = wait_for(related, EC.presence_of_element_located((By.ID, 'items')))
+        recs = wait_for(items, EC.presence_of_all_elements_located((By.TAG_NAME, 'ytd-compact-video-renderer')))
+
+        # recs = (driver.find_element(By.ID, 'related')
+        #         .find_element(By.ID, 'items')
+        #         .find_elements(By.TAG_NAME, "ytd-compact-video-renderer"))
     except:
         recs = []
 
@@ -158,9 +184,6 @@ def get_recommended(driver):
 
 
 def get_video_data(driver, video_url):
-    driver.get(video_url)
-    time.sleep(1)
-
     # if the page is asking for cookie consent, accept it first
     if driver.find_elements(By.TAG_NAME, "ytd-consent-bump-v2-lightbox"):
         print("There is a consent box, accept it")
@@ -171,16 +194,15 @@ def get_video_data(driver, video_url):
     video_likes = get_likes(driver)
     video_channel = get_channel(driver)
     video_desc = get_description(driver)
+    video_views = get_views(driver)
     video_transcript = get_transcript(driver)
     video_recs = get_recommended(driver)
 
-    return video_title, video_recs, video_desc, video_likes, video_channel, video_transcript
+    return video_title, video_recs, video_desc, video_views, video_likes, video_channel, video_transcript
 
 
 def main():
-    options = Options()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=Options())
+    driver = start_new_driver()
 
     graph = nx.DiGraph() # THIS HAS **URL IDS** AS IDS
     visited = set() # THIS IS A SET OF **URLS**
@@ -193,30 +215,43 @@ def main():
         print("Depth: ", d)
 
         for url in to_visit:
-            video_id = get_video_id(url)
+            try:
+                video_id = get_video_id(url)
+                print("Current video ID: " , video_id)
 
-            # if the video is already visited only get the new recommended videos from it, no need to reload other info
-            if video_id in [get_video_id(v) for v in visited]:
-                recommendations = get_recommended(driver)
-                print("Node already exists:", graph.nodes[video_id]["title"])
-            else:
-                title, recommendations, description, likes, channel, transcript = get_video_data(driver, url)
-                print("Title: ", title)
-                graph.add_node(video_id, url=url, title=title, depth=d, description=description, likes=likes, channel=channel, transcript=transcript)
+                # if the video is already visited only get the new recommended videos from it, no need to reload other info
+                if video_id in [get_video_id(v) for v in visited]:
+                    driver.get(url)
+                    time.sleep(1)
+                    recommendations = get_recommended(driver)
+                    print("Node already exists:", graph.nodes[video_id]["title"])
+                else:
+                    driver.get(url)
+                    time.sleep(1)
+                    title, recommendations, description, views, likes, channel, transcript = get_video_data(driver, url)
+                    print("Title: ", title)
+                    graph.add_node(video_id, url=url, title=title, depth=d, description=description, views=views, likes=likes, channel=channel, transcript=transcript)
 
-                # print("Recommendations: ", recommendations)
-                # print("Description: ", desc)
-                # print("Likes: ", likes)
-                # print("Channel: ", channel)
-                # print("Transcript: ", transcript)
+                    # print("Recommendations: ", recommendations)
+                    # print("Description: ", desc)
+                    # print("Likes: ", likes)
+                    # print("Channel: ", channel)
+                    # print("Transcript: ", transcript)
 
-            for recommendation in recommendations:
-                rec_id = get_video_id(recommendation)
-                graph.add_edge(video_id, rec_id)
-                if recommendation not in visited:
-                    to_visit_tmp.append(recommendation)
+                for recommendation in recommendations:
+                    rec_id = get_video_id(recommendation)
+                    graph.add_edge(video_id, rec_id)
+                    if recommendation not in visited:
+                        to_visit_tmp.append(recommendation)
 
-            visited.add(url)
+                visited.add(url)
+
+            # if the page takes way too long then reset the webdriver
+            except TimeoutException:
+                print(f"Timeout error, could not load {url}")
+                driver.quit()
+                driver = start_new_driver()
+                continue
 
         d += 1
         to_visit = to_visit_tmp
@@ -226,10 +261,11 @@ def main():
     print(to_visit)
     for recommendation in to_visit:
         driver.get(recommendation)
-        time.sleep(3)
+        time.sleep(1)
         video_id = get_video_id(recommendation)
         title = get_video_title(driver)
         desc = get_description(driver)
+        views = get_views(driver)
         likes = get_likes(driver)
         channel = get_channel(driver)
         transcript = get_transcript(driver)
@@ -240,7 +276,7 @@ def main():
         if video_id in [get_video_id(v) for v in visited]:
             print("Node already exists:", graph.nodes[video_id]["title"])
         else:
-            graph.add_node(video_id, url=recommendation, title=title, depth=d, description=desc, likes=likes, channel=channel, transcript=transcript)
+            graph.add_node(video_id, url=recommendation, title=title, depth=d, description=desc, views=views, likes=likes, channel=channel, transcript=transcript)
 
     driver.quit()
 
